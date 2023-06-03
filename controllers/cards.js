623490013130863fdfa1b5ffff40cc6c6e36955c
Konstantin Radchenko <kostya.rad3@yandex.ru>
2023-06-03 13:11:34 +0300

    fix: fixed errors

--- controllers/cards.js
+++ controllers/cards.js
@@ -1,9 +1,6 @@
 /* eslint-disable no-unused-vars */
 const Card = require('../models/card');
-
-const ERROR_BAD_REQUEST = 400;
-const ERROR_NOT_FOUND = 404;
-const ERROR_SERVER = 500;
+const { ERROR_BAD_REQUEST, ERROR_NOT_FOUND, ERROR_SERVER } = require('../utils/constants');
 
 function getCards(req, res) {
   Card.find({})
@@ -19,7 +16,7 @@ function createCard(req, res) {
   Card.create({ name, link, owner: req.user._id })
     .then((card) => res.send({ data: card }))
     .catch((err) => {
-      if (err.message.includes('validation failed')) {
+      if (err.name === 'ValidationError') {
         res.status(ERROR_BAD_REQUEST).send({ message: 'Переданы некорректные данные при создании карточки.' });
         return;
       }
@@ -29,11 +26,19 @@ function createCard(req, res) {
 
 function deleteCard(req, res) {
   Card.findByIdAndRemove(req.params.cardId)
-    .then((card) => res.send({ data: card }))
-    .catch((err) => {
-      if (err.message === 'Not found') {
-        res.status(ERROR_NOT_FOUND).send({ message: 'Пользователь по указанному _id не найден.' });
+    .then((card) => {
+      if (!card) {
+        res.send({ message: 'Такой карточки не существует.' });
+        return;
       }
+      res.send({ data: card });
+    })
+    .catch((err) => {
+      if (err.name === 'CastError') {
+        res.status(ERROR_NOT_FOUND).send('Передан несуществующий _id карточки.');
+        return;
+      }
+      res.status(ERROR_SERVER).send({ message: 'На сервере произошла ошибка' });
     });
 }
 
@@ -43,14 +48,16 @@ function setLike(req, res) {
     { $addToSet: { likes: req.user._id } },
     { new: true },
   )
-    .then((card) => res.send({ data: card }))
-    .catch((err) => {
-      if (err.message.includes('validation failed')) {
-        res.status(ERROR_BAD_REQUEST).send({ message: 'Переданы некорректные данные для постановки/снятии лайка.' });
+    .then((card) => {
+      if (!card) {
+        res.send({ message: 'Такой карточки не существует.' });
         return;
       }
-      if (err.message === 'Not found') {
-        res.status(ERROR_NOT_FOUND).send({ message: 'Передан несуществующий _id карточки.' });
+      res.send({ data: card });
+    })
+    .catch((err) => {
+      if (err.name === 'CastError') {
+        res.status(ERROR_NOT_FOUND).send('Передан несуществующий _id карточки.');
         return;
       }
       res.status(ERROR_SERVER).send({ message: 'На сервере произошла ошибка' });
@@ -63,14 +70,16 @@ function removeLike(req, res) {
     { $pull: { likes: req.user._id } },
     { new: true },
   )
-    .then((card) => res.send({ data: card }))
-    .catch((err) => {
-      if (err.message.includes('validation failed')) {
-        res.status(ERROR_BAD_REQUEST).send({ message: 'Переданы некорректные данные для постановки/снятии лайка.' });
+    .then((card) => {
+      if (!card) {
+        res.send({ message: 'Такой карточки не существует.' });
         return;
       }
-      if (err.message === 'Not found') {
-        res.status(ERROR_NOT_FOUND).send({ message: 'Передан несуществующий _id карточки.' });
+      res.send({ data: card });
+    })
+    .catch((err) => {
+      if (err.name === 'CastError') {
+        res.status(ERROR_NOT_FOUND).send('Передан несуществующий _id карточки.');
         return;
       }
       res.status(ERROR_SERVER).send({ message: 'На сервере произошла ошибка' });
--- routes/index.js
+++ routes/index.js
@@ -5,4 +5,6 @@ const cardRoutes = require('./cards');
 router.use('/users', userRoutes);
 router.use('/cards', cardRoutes);
 
+router.use('*', (req, res) => { res.status(404).send('Page not found'); });
+
 module.exports = router;
