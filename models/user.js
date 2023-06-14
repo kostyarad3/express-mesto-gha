bce35c569d936e9d9e518ed3f835ea2f3bed6975
Konstantin Radchenko <kostya.rad3@yandex.ru>
2023-06-14 22:34:30 +0300

    7

--- models/user.js
+++ models/user.js
@@ -38,43 +38,24 @@ const userSchema = new mongoose.Schema({
       message: 'Нужно ввести URL',
     },
   },
-  statics: {
-    findUserByCredentials(email, password) {
-      return this.findOne({ email }).select('+password')
-        .then((user) => {
-          if (!user) {
+});
+
+userSchema.statics.findUserByCredentials = function (email, password) {
+  return this.findOne({ email }).select('+password')
+    .then((user) => {
+      if (!user) {
+        return Promise.reject(new Error('Неправильные почта или пароль'));
+      }
+
+      return bcrypt.compare(password, user.password)
+        .then((matched) => {
+          if (!matched) {
             return Promise.reject(new Error('Неправильные почта или пароль'));
           }
 
-          return bcrypt.compare(password, user.password)
-            .then((matched) => {
-              if (!matched) {
-                return Promise.reject(new Error('Неправильные почта или пароль'));
-              }
-
-              return user;
-            });
+          return user;
         });
-    },
-  },
-});
-
-// userSchema.statics.findUserByCredentials = function (email, password) {
-//   return this.findOne({ email }).select('+password')
-//     .then((user) => {
-//       if (!user) {
-//         return Promise.reject(new Error('Неправильные почта или пароль'));
-//       }
-
-//       return bcrypt.compare(password, user.password)
-//         .then((matched) => {
-//           if (!matched) {
-//             return Promise.reject(new Error('Неправильные почта или пароль'));
-//           }
-
-//           return user;
-//         });
-//     });
-// };
+    });
+};
 
 module.exports = mongoose.model('user', userSchema);
